37b423efdc2451ce77126a7054238cd4abcc0a6c
Logan Wilquin <Skullyfox.yt@gmail.com>
2023-02-24 11:48:43 +0100

    First Commit

---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<script setup>
+    import {ref} from 'vue';
+
+    let isActive = ref(false);
+
+    const mobileButtonHandler = (e) => {
+        isActive.value = !isActive.value;
+    }
+</script>
+<template>
+    <main>
+        <div class="navigation">
+            <div class="mobileNavButton">
+                <i  class="fas" 
+                    :class="{'fa-bars': !isActive, 'fa-xmark': isActive}" 
+                    @click="mobileButtonHandler">
+                </i>
+            </div>
+            <div class="left">
+                <NuxtLink to="#">
+                    <img src="../assets/img/logo.png" alt="Onivoid Logo">
+                </NuxtLink>
+            </div>
+            <nav class="middle">
+                <ul>
+                    <li>
+                        <NuxtLink to="#">Accueil</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#">Réseaux</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#">Portfolio</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#">Shop</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="#">Contact</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+            <div class="right">
+                <NuxtLink to="#">
+                    <img src="../assets/img/discord_logo.png" alt="Discord Logo">
+                    <span>
+                        <p class="small">Rejoins</p>
+                        <p class="big">La Taverne</p>
+                    </span>
+                </NuxtLink>
+            </div>
+        </div>
+        <div class="mobileNavContainer" :class="{'isActive': isActive}">
+            <ul>
+                <li>
+                    <NuxtLink to="#" @click="mobileButtonHandler">Accueil</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="#" @click="mobileButtonHandler">Réseaux</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="#" @click="mobileButtonHandler">Portfolio</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="#" @click="mobileButtonHandler">Shop</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="#" @click="mobileButtonHandler">Contact</NuxtLink>
+                </li>
+            </ul>
+        </div>
+      <slot />
+    </main>
+</template>
+
+<style scoped>
+    @import url('../assets/css/layouts/default_global.css');
+    @import url('../assets/css/layouts/default_desktop.css');
+    @import url('../assets/css/layouts/default_tablet.css');
+    @import url('../assets/css/layouts/default_mobile.css');
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<script setup>
+    definePageMeta({
+        layout: "default",
+    });
+</script>
+
+<template>
+    <header>
+        <div class="headerContainer">
+            <TwtichPlayer channel="onivoid_"></TwtichPlayer>
+        </div>
+        <div class="favorite">
+
+        </div>
+    </header>
+    <section class="favorite">
+        <h1>Jeux Favorits</h1>
+        <div class="favContainer">
+            <div class="item">
+                <img 
+                    src="https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png" 
+                    alt="">
+                <p class="title">Titre</p>
+                <p class="type">genre</p>
+            </div>
+            <div class="item">
+                <img 
+                    src="https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png" 
+                    alt="">
+                <p class="title">Titre</p>
+                <p class="type">genre</p>
+            </div>
+            <div class="item">
+                <img 
+                    src="https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png" 
+                    alt="">
+                <p class="title">Titre</p>
+                <p class="type">genre</p>
+            </div>
+            <div class="item">
+                <img 
+                    src="https://caer.univ-amu.fr/wp-content/uploads/default-placeholder.png" 
+                    alt="">
+                <p class="title">Titre</p>
+                <p class="type">genre</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+    @import url('../assets/css/pages/index/desktop.css');
+    @import url('../assets/css/pages/index/tablet.css');
+    @import url('../assets/css/pages/index/mobile.css');
+</style>
